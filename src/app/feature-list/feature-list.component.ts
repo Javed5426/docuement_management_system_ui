5e9f4f41f5eb158d717bcf3ac212fbcd9a8a8a39
Javed <javedkhan1078692@gmail.com>
2024-08-20 11:30:48 +0530

    download and delete feature added

--- src/app/feature-list/feature-list.component.ts
+++ src/app/feature-list/feature-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FeaturelistService } from '../featurelist/featurelist.service';
 
 @Component({
@@ -11,7 +11,8 @@ export class FeatureListComponent implements OnInit {
   data: any = "";
 
   constructor(private router: Router,
-    private _service:FeaturelistService
+    private _service:FeaturelistService,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
@@ -25,6 +26,16 @@ export class FeatureListComponent implements OnInit {
     this._service.getAllData(payload).subscribe( res=>{
       this.data = res.response;
     });
+  }
+
+  downloadFileById(id:any){
+    this._service.downloadFile(id).subscribe(res=>{
+      console.log("file downloaded")
+    })
+  }
+
+  deletedDocument(id:any){
+    console.log("File being downloaded." , id)
   }
 
 }
--- src/app/featurelist/featurelist.service.ts
+++ src/app/featurelist/featurelist.service.ts
@@ -7,12 +7,16 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FeaturelistService {
-
+  
   constructor(private _http:HttpClient) { }
 
   private commonUrl = "http://localhost:8091";
 
   getAllData(data:any):Observable<any>{
     return this._http.post(this.commonUrl + '/myDocuments' + '/searchData', data);
+  }
+
+  downloadFile(id: any) {
+    return this._http.post(this.commonUrl + '/myDocuments' + `/download/${id}`, '')
   }
 }
